cf49ad7e87ef2e2c7106f2d56d35439e04a22635
Sarah H <sarahrose.henderson@gmail.com>
2019-09-13 03:20:26 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  # array = [1,4,5,6,3]
+array.sort do |a, b|
+  a <=> b
+end
+end
+array = [1,4,5,6,3]
+sort_array_asc(array)
 
+def sort_array_desc(array)
+  # array = [1,4,5,6,3]
+array.sort do |a, b|
+  b <=> a
+end
+end
+array = [1,4,5,6,3]
+sort_array_desc(array)
+
+def sort_array_char_count(array)
+array.sort_by {|obj| obj.size}
+end
+array = ["cat", "horse", "frog", "elephant"]
+sort_array_char_count(array)
+
+
+array = ["blake", "ashley", "scott"]
+def swap_elements(array)
+array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+swap_elements(array)
+
+def reverse_array(array)
+array.reverse
+end
+array = [3, 4, 9, 2]
+reverse_array(array)
+
+def kesha_maker(array)
+
+end
